928624d8f52721ade1cc87bc0c204e44c996437a
fanrui07 <fanrui07@maoyan.com>
2020-12-11 02:53:29 +0800

    del

--- app/src/main/java/com/fr1014/mycoludmusic/http/api/ApiConstants.java
+++
@@ -1,6 +0,0 @@
-package com.fr1014.mycoludmusic.http.api;
-
-public interface ApiConstants {
-    public static String BASE_URL_WYY = "https://api.mtnhao.com/";
-    public static String BASE_URL_KW = "http://antiserver.kuwo.cn/";
-}
