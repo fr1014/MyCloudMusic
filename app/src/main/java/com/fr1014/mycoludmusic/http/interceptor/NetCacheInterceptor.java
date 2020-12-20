38de64cf0d6154400c1a72f69d50cd85557067ff
fanrui07 <fanrui07@maoyan.com>
2020-12-20 19:32:16 +0800

    请求歌曲url不做缓存策略

--- app/src/main/java/com/fr1014/mycoludmusic/http/interceptor/NetCacheInterceptor.java
+++ app/src/main/java/com/fr1014/mycoludmusic/http/interceptor/NetCacheInterceptor.java
@@ -1,6 +1,9 @@
 package com.fr1014.mycoludmusic.http.interceptor;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import okhttp3.Interceptor;
 import okhttp3.Request;
@@ -11,11 +14,21 @@ import okhttp3.Response;
  * Describe:自定义okHttp缓存策略( 有网时的缓存 )
  */
 public class NetCacheInterceptor implements Interceptor {
+    static final List<String> filterPaths = new ArrayList<>();
+
+    static {
+        filterPaths.add("/anti.s");
+        filterPaths.add("/song/url");
+    }
 
     @Override
     public Response intercept(Chain chain) throws IOException {
         Request request = chain.request();
+        String path = request.url().url().getPath();
         Response response = chain.proceed(request);
+        if (filterPaths.contains(path)) {
+            return response;
+        }
         int onlineCacheTime = 60;//在线的时候的缓存过期时间，如果想要不缓存，直接时间设置为0(单位：秒)
         return response.newBuilder()
                 .header("Cache-Control", "public, max-age=" + onlineCacheTime)
