b8dc43c57eedf38ec4578e565584535e6073270f
fanrui07 <fanrui07@maoyan.com>
2021-02-06 18:48:44 +0800

    fix: 每日推荐列表展示问题

--- app/src/main/java/com/fr1014/mycoludmusic/ui/home/homepage/dayrecommend/DayRecommendAdapter.kt
+++ app/src/main/java/com/fr1014/mycoludmusic/ui/home/homepage/dayrecommend/DayRecommendAdapter.kt
@@ -13,7 +13,7 @@ import com.fr1014.mycoludmusic.musicmanager.AudioPlayer
 import com.fr1014.mycoludmusic.musicmanager.Music
 import com.fr1014.mycoludmusic.utils.glide.GlideApp
 
-class DayRecommendAdapter(layoutResId: Int) : BaseAdapter<SongsBean, BaseViewHolder>(layoutResId) ,BaseAdapter.OnItemClickListener{
+class DayRecommendAdapter(layoutResId: Int) : BaseAdapter<SongsBean, BaseViewHolder>(layoutResId), BaseAdapter.OnItemClickListener {
 
     init {
         onItemClickListener = this
@@ -25,25 +25,25 @@ class DayRecommendAdapter(layoutResId: Int) : BaseAdapter<SongsBean, BaseViewHol
                 .load(data.al.picUrl + "?param=200y200")
                 .apply(RequestOptions().placeholder(R.drawable.ic_placeholder).centerCrop().transform(RoundedCorners(30)))
                 .into(holder.getView(R.id.iv_cover))
-        holder.getView<TextView>(R.id.tv_title).text = data.name
-        holder.getView<TextView>(R.id.tv_info).text = "${data.getArInfo()} - ${data.al.name}"
-        holder.addOnClickListener(R.id.cl_item_recommend)
-        if (holder.adapterPosition == (itemCount - 1)){
-            holder.getView<View>(R.id.view_place_holder).visibility = View.VISIBLE
+        holder.apply {
+            getView<TextView>(R.id.tv_title).text = data.name
+            getView<TextView>(R.id.tv_info).text = "${data.getArInfo()} - ${data.al.name}"
+            addOnClickListener(R.id.cl_item_recommend)
+            getView<View>(R.id.view_place_holder).visibility = if (holder.layoutPosition == (itemCount - 1)) View.VISIBLE else View.GONE
         }
     }
 
     override fun onItemClick(adapter: BaseAdapter<*, *>, view: View, position: Int) {
-        when(view.id){
-            R.id.cl_item_recommend ->{
+        when (view.id) {
+            R.id.cl_item_recommend -> {
                 val data = getData(position)
-                AudioPlayer.get().addAndPlay(Music(data.id,data.getArInfo(),data.name,"","",""))
+                AudioPlayer.get().addAndPlay(Music(data.id, data.getArInfo(), data.name, "", "", ""))
             }
         }
     }
 }
 
-fun SongsBean.getArInfo() : String{
+fun SongsBean.getArInfo(): String {
     val sb = StringBuilder()
     for (i in ar.indices) {
         ar[i].apply {
