3a9fc14090c34263dfb7f3f1a36d1a60032ee709
fanrui07 <fanrui07@maoyan.com>
2021-02-24 15:31:26 +0800

    fix：判断条件错误

--- app/src/main/java/com/fr1014/mycoludmusic/utils/MusicUtils.kt
+++ app/src/main/java/com/fr1014/mycoludmusic/utils/MusicUtils.kt
@@ -1,5 +1,6 @@
 package com.fr1014.mycoludmusic.utils
 
+import android.text.TextUtils
 import com.fr1014.mycoludmusic.musicmanager.Music
 
 object MusicUtils {
@@ -8,6 +9,6 @@ object MusicUtils {
      * 判断两首歌曲是否为同一首歌曲
      */
     fun isSameMusic(first: Music, second: Music) :Boolean{
-       return ((first.id != 0L && first.id == second.id) || (first.musicrid != null && first.musicrid == second.musicrid))
+       return ((first.id != 0L && first.id == second.id) || (!TextUtils.isEmpty(first.musicrid) && first.musicrid == second.musicrid))
     }
 }
